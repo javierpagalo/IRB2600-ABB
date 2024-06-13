f184f931348cef6b088acf565079ec6e9e1200f6
Juan Saeteros <jumasaet@espol.edu.ec>
2024-06-13 10:26:50 -0500

    Cambios para enviar a writing_node el tamaño del pen

--- irb2600_master/src/writing_node.py
+++ irb2600_master/src/writing_node.py
@@ -52,13 +52,6 @@ marker.lifetime = rospy.Duration(50)
 #Recibimos del Subscriber un msg de tipo JointState de moveit y posteriormente lo publicamos con el Publisher como goal
 def state_position(goal_state: JointState):
     global pos
-    # if (goal_state.position != pos ):
-    #     goal_state.velocity = [10, 10, 10, 10, 10, 10]
-    #     pub.publish(goal_state)
-    #     pos = goal_state.position
-    # j_array = np.array(pos)*180/math.pi
-    # pos = list(j_array)
-    #write_data(pos, "goals") #Funcion para guardar los datos
     if fig != '_none': 
         plan_marker()
     else:
@@ -80,65 +73,20 @@ def plan_marker():
         marker_array.markers.append(marker)
         marker_pub.publish(marker_array)
 
-#Recibimos un msg de tipo JointState a traves del topico real_joint_state y lo guardamos para posteriormente hacer un controlador
-def real_callback(real_state: JointState):
-    real_position(real_state)
-    #real_torque  (real_state)
-    #real_velocity(real_state)
-
-def real_position(real_state: JointState): 
-    global real_pos
-    j_array = np.array(real_state.position)*180/math.pi
-    real_pos = list(j_array)
-    write_data(real_pos, "real_states") #Funcion para guardar los datos
-    
-def real_torque(real_state: JointState): 
-    global torque
-    j_array = np.array(real_state.effort)
-    torque= list(j_array)
-    write_data(torque, "real_torques") #Funcion para guardar los datos
-
-def real_velocity(real_state: JointState):
-    global real_vel
-    j_array = np.array(real_state.velocity)
-    real_vel= list(j_array)
-    write_data(real_vel, "real_velocities") #Funcion para guardar los datos
-
 def figure(data_figure : str):
     global fig
     global pen
     if "," in str(data_figure.data):
         fig = (str(data_figure.data),",").split(",")[0]
-        pen = (str(data_figure.data),",").split(",")[1]
+        pen = float((str(data_figure.data),",").split(",")[1])
     else:
         fig = str(data_figure.data)
-def write_data(posiciones, pre):
-    global new_file
-    if fig != '_none':
-        if os.path.exists(os.path.join(ruta_file, "joint_" + pre + fig + ".txt")):
-            if not new_file:
-                archivo = open(os.path.join(ruta_file, "joint_" + pre + fig + ".txt"), "w")
-                archivo.write("Joint0,Joint1,Joint2,Joint3,Joint4,Joint5 \n" + ",".join(np.array(posiciones,str)) + "\n")
-                archivo.close()
-                new_file = True
-            else:
-                archivo = open(os.path.join(ruta_file, "joint_" + pre + fig + ".txt"), "a")
-                archivo.write(",".join(np.array(posiciones,str)) + "\n")
-                archivo.close()
-        else:
-            archivo = open(os.path.join(ruta_file, "joint_" + pre + fig + ".txt"), "w")
-            archivo.write("Joint0,Joint1,Joint2,Joint3,Joint4,Joint5 \n" + ",".join(np.array(posiciones,str)) + "\n")
-            archivo.close()
-            new_file = True
-    
 
 if __name__ == "__main__":
     rospy.init_node("writing_node")
     moveit_commander.roscpp_initialize(sys.argv)
-#    pub             = rospy.Publisher ("/joint_goals" , JointState, queue_size=10)
     marker_pub      = rospy.Publisher("/visualization_marker_array", MarkerArray, queue_size = 2)
     subGoalState    = rospy.Subscriber("/joint_states", JointState, callback = state_position)
-#    subRealState    = rospy.Subscriber("/real_joint_states", JointState, callback = real_callback)
     subWritingData  = rospy.Subscriber("/figure_writing", String, callback = figure)
     group           = moveit_commander.MoveGroupCommander("robot_arm")
 
